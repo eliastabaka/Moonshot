da4222758d3c69a3ab4caae86bf7c25600310323
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-08-30 23:00:27 +0100

    Using Generics to load any time of Codable data

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Milosz Tabaka on 30/08/2022.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
